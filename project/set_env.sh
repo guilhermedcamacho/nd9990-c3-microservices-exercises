bba9e50707596f173ca76f3ee484814cb8bc855d
Guilherme Camacho <guilherme.camacho@mv.com.br>
2020-11-03 23:52:04 -0300

    Refactoring the API into 2 microservice projects

--- project/set_env.sh
+++ project/set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=
-export POSTGRES_PASSWORD=
-export POSTGRES_HOST=
-export POSTGRES_DB=
-export AWS_BUCKET=
-export AWS_REGION=
-export AWS_PROFILE=
-export JWT_SECRET=
+export POSTGRES_USERNAME=camacho
+export POSTGRES_PASSWORD=camacho9834
+export POSTGRES_HOST=cdr251747298464dev.cjtd7tovtfpg.sa-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=test-251747298464-dev
+export AWS_REGION=sa-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=udagram9834
 export URL=http://localhost:8100
